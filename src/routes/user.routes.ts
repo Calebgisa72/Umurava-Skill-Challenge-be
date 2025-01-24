d3e57c5a4e914e23f8b58b423891d6ce03b9757b
Gisa M. Caleb Pacifique <gisacaleb72@gmail.com>
2025-01-24 13:33:39 +0200

    ft-seeding-users

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,15 +1,10 @@
-import { Router, Request, Response } from 'express';
+import { Router } from 'express';
 import { loginUser, signupUser } from '../controllers/user.contollers';
-import { successResponse } from '../utils/responses.utils';
 
 const router = Router();
 
-router.get('/', (req: Request, res: Response) => {
-    successResponse(res, 200, 'user routes');
-    return;
-  });
-
 router.post('/register', signupUser);
 router.post('/login', loginUser);
+// router.put('/profile', updateProfile);
 
 export default router;
